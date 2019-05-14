6f8a50636a464d96d2ca2679c44e248707350cdb
liangxiegame <liangxiegame@163.com>
2019-05-14 18:17:52 +0800

    create project

---
+++ Assets/Scripts/App.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace PomodoroApp
+{
+    public class App : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
